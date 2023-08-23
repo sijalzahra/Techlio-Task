98931ee0e0b12adf42a0a82219f9426dd226a884
Sijal Zahra <syedasijalzahra@gmail.com>
2023-08-23 23:01:05 +0500

    commit

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 document.addEventListener('scroll', () => {
     const header = document.querySelector('header');
+    const logo = header.querySelector('.logo');
 
     if (window.scrollY > 0){
         header.classList.add('scrolled');
+        logo.src = 'images/techlio-logo-color-change.png';
     } else {
         header.classList.remove('scrolled');
+        logo.src = "images/logo.png";
     }
 });
